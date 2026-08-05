94636a25db9fc5a2a1d908e43a74e8214005724d
josh10richards <joshua10richards@gmail.com>
2026-08-05 14:18:10 +0100

    setting up movement and mouse looking around on the old input manager system

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 200f;
+    float xRotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")*sensitivity*Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
